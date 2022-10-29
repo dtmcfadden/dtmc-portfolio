24b62a9f8bde1f6bb0de9b7167baa12188e55f0f
whodedo <dtmc81@gmail.com>
2022-10-29 15:35:23 +0700

    removed old mysql files

--- database/mysql/dal/types.ts
+++
@@ -1,8 +0,0 @@
-interface ListFilters {
-	isDeleted?: boolean;
-	includeDeleted?: boolean;
-}
-
-export interface GetAllUsersFilters extends ListFilters {}
-export interface GetAllSessionFilters extends ListFilters {}
-export interface GetAllAccountFilters extends ListFilters {}
