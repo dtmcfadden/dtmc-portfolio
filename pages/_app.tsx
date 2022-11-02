3191c8affb4a84518e41bd03f8751859fdb27adf
whodedo <dtmc81@gmail.com>
2022-11-02 16:25:56 +0700

    Added Recoil package with folders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import { SessionProvider } from 'next-auth/react';
 import { Container } from 'react-bootstrap';
 import Head from 'next/head';
 import { useEffect } from 'react';
-// import { Component } from 'react';
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
 	// console.log('App pageProps', pageProps);
@@ -18,18 +18,20 @@ export default function App({ Component, pageProps }: AppProps) {
 	// }, []);
 
 	return (
-		<SSRProvider>
-			{/* <SessionProvider session={session}> */}
-			<SessionProvider session={pageProps.session}>
-				<Head>
-					<meta name="viewport" content="width=device-width, initial-scale=1" />
-				</Head>
-				<Container>
-					<Layout>
-						<Component {...pageProps} />
-					</Layout>
-				</Container>
-			</SessionProvider>
-		</SSRProvider>
+		<RecoilRoot>
+			<SSRProvider>
+				{/* <SessionProvider session={session}> */}
+				<SessionProvider session={pageProps.session}>
+					<Head>
+						<meta name="viewport" content="width=device-width, initial-scale=1" />
+					</Head>
+					<Container>
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					</Container>
+				</SessionProvider>
+			</SSRProvider>
+		</RecoilRoot>
 	);
 }
