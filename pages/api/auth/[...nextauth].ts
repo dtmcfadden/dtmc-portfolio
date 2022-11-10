de24409c6aa659018b6151cb715da7de9fbe856a
whodedo <dtmc81@gmail.com>
2022-11-10 11:43:06 +0700

    Added google provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -64,10 +64,10 @@ export const authOptions: NextAuthOptions = {
 			// @ts-ignore
 			scope: 'read:user',
 		}),
-		// GoogleProvider({
-		// 	clientId: process.env.GOOGLE_ID || '',
-		// 	clientSecret: process.env.GOOGLE_SECRET || '',
-		// }),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID || '',
+			clientSecret: process.env.GOOGLE_SECRET || '',
+		}),
 		// TwitterProvider({
 		//   clientId: process.env.TWITTER_ID || '',
 		//   clientSecret: process.env.TWITTER_SECRET || '',
